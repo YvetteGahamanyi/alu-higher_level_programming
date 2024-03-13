b56c6cb7f4abae34fafad7276d8f2943415a8509
Gahamanyi-dev <ygahamanyi26@gmail.com>
2024-03-14 01:29:04 +0200

    fix line too long

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -69,7 +69,7 @@ class Square:
         Raises:
             TypeError: If position is not a tuple of 2 positive integers.
         """
-        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(i, int) for i in value) or not all(i >= 0 for i in value):
+        if not all(isinstance(v, int) and v >= 0 for v in value) or not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
 
         self.__position = value
