b7091431c6d85e187e97da9ec41a36e2d0e538fb
Gahamanyi-dev <ygahamanyi26@gmail.com>
2024-03-24 14:04:16 +0200

    something is wrong

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """__summary__
-- python scripts that fetches https://intranet.hbtn.io/status
+- python scripts that fetches https://alu-intranet.hbtn.io/status
 """
 
 import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get("https://alu-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
