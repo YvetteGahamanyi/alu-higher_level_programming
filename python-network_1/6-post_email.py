45c7d53b65b19cc2b1908f2fe6bf9e18bc395730
Gahamanyi-dev <ygahamanyi26@gmail.com>
2024-03-24 13:13:15 +0200

    POST an email #1

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""__summary__
+- takes in a URL and email
+- sends a POST request to the passed URL with the email as a parameter
+- displays the body of the response (decoded in utf-8)
+"""
+
+import sys
+import requests
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+
+    reqs = requests.post(url, data=value)
+    print(reqs.text)
+
