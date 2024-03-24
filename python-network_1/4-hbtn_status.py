d1762db63e5387d919df4bfa241f487eb096bc49
Gahamanyi-dev <ygahamanyi26@gmail.com>
2024-03-24 13:45:50 +0200

    try something

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 """__summary__
-- takes in a URL
-- sends a request to the URL and
-- displays the body of the response (decoded in utf-8).
+- python scripts that fetches https://intranet.hbtn.io/status
 """
 
 import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://alu-intranet.hbtn.io/status")
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
