f5b10fd146215e238b1701ef569d6051540b0dea
Gahamanyi-dev <ygahamanyi26@gmail.com>
2024-03-24 13:48:58 +0200

    expected

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -5,9 +5,11 @@
 
 import requests
 
-
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    r = requests.get("https://intranet.hbtn.io/status", headers=headers)
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
