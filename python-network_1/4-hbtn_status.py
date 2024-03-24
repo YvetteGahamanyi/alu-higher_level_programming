39f10c3a17ba24cd7eb6ed17c4a0a91a27ca3aba
Gahamanyi-dev <ygahamanyi26@gmail.com>
2024-03-24 14:03:02 +0200

    back to normal

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -2,14 +2,12 @@
 """__summary__
 - python scripts that fetches https://intranet.hbtn.io/status
 """
+
 import requests
 
-if __name__ == '__main__':
 
-    res = requests.get("https://intranet.hbtn.io/status")
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(res.text)))
-    if res.status_code == 200:
-        print("\t- content: OK")
-    else:
-        print("\t- content: Unexpected status code: {}".format(res.status_code))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
